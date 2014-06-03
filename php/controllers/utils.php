e3c1f61828822ddb17bfc417da2391f542d97c6e
Tim Schroeder <timschroeder@vidder.com>
2014-06-02 23:12:01 -0700

    fix template path

--- php/controllers/utils.php
+++ php/controllers/utils.php
@@ -5,8 +5,8 @@ function render_page($page, $title) {
     "title" => $title,
     "page" => $_SERVER["REQUEST_URI"]
     );
-  Flight::render('../layout/header', $header_params, 'header');
-  Flight::render('../layout/footer', array(), 'footer');
+  Flight::render('layout/header', $header_params, 'header');
+  Flight::render('layout/footer', array(), 'footer');
   Flight::render($page, array());
 }
 
