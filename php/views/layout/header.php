1727731e7c9792cefcbb2363861cbbf561e206e0
Tim Schroeder <timschroeder@vidder.com>
2014-06-02 23:22:24 -0700

    Added button to pages

--- php/views/layout/header.php
+++ php/views/layout/header.php
@@ -2,8 +2,8 @@
 <html lang="en">
   <head>
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="/css/style.css" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
@@ -13,5 +13,6 @@
         <a href="/" class="btn btn-default <?php if ($page == "/") { echo "active"; } ?>" role="button">Home</a>
         <a href="/proposal-story" class="btn btn-default <?php if ($page == "/proposal-story") { echo "active"; } ?>" role="button">Proposal Story</a>
         <a href="http://www.myregistry.com/public/schroederwedding" target="_blank" class="btn btn-default" role="button">Registry</a>
+        <a href="/recipes" class="btn btn-default <?php if ($page == "/recipes") { echo "active"; } ?>" role="button">Recipes</a>
         <a href="/RSVP" class="btn btn-default <?php if ($page == "/RSVP") { echo "active"; } ?>" role="button">RSVP</a>
       </div>
