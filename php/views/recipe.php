ad6a4195b805f203c05e954130a64ef8cbf558e6
Tim Schroeder <timschroeder@vidder.com>
2014-06-05 21:41:59 -0700

    Better look for recipe display

--- php/views/recipe.php
+++ php/views/recipe.php
@@ -1,9 +1,36 @@
 <?php echo $header; ?>
 <h1>Recipe</h1>
 
-Display recipe here
-
-Title: <?php echo $recipe['title']; ?>
-etc.
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <span><?php echo $recipe['title']; ?></span>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <h3>Ingredients</h3>
+    <p><?php echo $recipe['ingredients']; ?></p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <h3>Directions</h3>
+    <p><?php echo $recipe['directions']; ?></p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-10 col-md-offset-1">
+    <h3>Notes</h3>
+    <p><?php echo $recipe['notes']; ?></p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-5 col-md-offset-1">
+    <span>Original Author: <?php echo $recipe['owner']; ?></span>
+  </div>
+  <div class="col-md-5 col-md-offset-1">
+    <span>Submitter: <?php echo $recipe['submitter']; ?></span>
+  </div>
+</div>
 
 <?php echo $footer; ?>
