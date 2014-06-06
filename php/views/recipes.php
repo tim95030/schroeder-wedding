8ad9556c497ec4c76afabe080dea76166b176ace
Tim Schroeder <timschroeder@vidder.com>
2014-06-05 18:49:34 -0700

    fixed typo and added some required field markes

--- php/views/recipes.php
+++ php/views/recipes.php
@@ -4,7 +4,7 @@
 <form type="POST" action="recipes/add" role="form">
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" placeholder="Enter Recipe Title" />
+    <input required type="text" class="form-control" id="title" placeholder="Enter Recipe Title" />
   </div>
   <div class="form-group">
     <label for="description">Description</label>
@@ -12,15 +12,15 @@
   </div>
   <div class="form-group">
     <label for="ingredients">Ingredients</label>
-    <textarea id="ingredients" rows="4" class="form-control"></textarea>
+    <textarea required id="ingredients" rows="4" class="form-control" placeholder="Enter Ingredients"></textarea>
   </div>
   <div class="form-group">
     <label for="directions">Directions</label>
-    <textarea id="directions" rows="4" class="form-controle"></textarea>
+    <textarea required id="directions" rows="4" class="form-control" placeholder="Enter Recipe Directions"></textarea>
   </div>
   <div class="form-group">
     <label for="notes">Notes</label>
-    <textarea id="notes" rows="2" class="form-control"></textarea>
+    <textarea id="notes" rows="2" class="form-control" placeholder="Enter any optional notes"></textarea>
   </div>
   <div class="form-group">
     <label for="owner">Original Recipe Author</label>
@@ -28,7 +28,7 @@
   </div>
   <div class="form-group">
     <label for="submitter">Submitter</label>
-    <input type="text" class="form-control" id="submitter" placeholder="Enter your name" />
+    <input required type="text" class="form-control" id="submitter" placeholder="Enter your name" />
   </div>
 </form>
 
