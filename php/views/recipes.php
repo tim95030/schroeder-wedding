7cefd017d356119bd3f5bddfbfca0d6b82bca792
Tim Schroeder <timschroeder@vidder.com>
2014-06-05 21:10:28 -0700

    Try using name to fix form

--- php/views/recipes.php
+++ php/views/recipes.php
@@ -1,37 +1,37 @@
 <?php echo $header; ?>
 <h1>Recipes</h1>
 
-<form method="POST" action="recipes/add" role="form">
+<form method="post" action="recipes/add" role="form">
   <div class="form-group">
     <label for="title">Title</label>
-    <input required type="text" class="form-control" id="title" placeholder="Enter Recipe Title" />
+    <input required type="text" class="form-control" name="title" id="title" placeholder="Enter Recipe Title" />
   </div>
   <div class="form-group">
     <label for="description">Description</label>
-    <input type="text" class="form-control" id="description" placeholder="Enter Recipe Description" />
+    <input type="text" class="form-control" name="description" id="description" placeholder="Enter Recipe Description" />
   </div>
   <div class="form-group">
     <label for="ingredients">Ingredients</label>
-    <textarea required id="ingredients" rows="4" class="form-control" placeholder="Enter Ingredients"></textarea>
+    <textarea required name="ingredients" id="ingredients" rows="4" class="form-control" placeholder="Enter Ingredients"></textarea>
   </div>
   <div class="form-group">
     <label for="directions">Directions</label>
-    <textarea required id="directions" rows="4" class="form-control" placeholder="Enter Recipe Directions"></textarea>
+    <textarea required name="directions" id="directions" rows="4" class="form-control" placeholder="Enter Recipe Directions"></textarea>
   </div>
   <div class="form-group">
     <label for="notes">Notes</label>
-    <textarea id="notes" rows="2" class="form-control" placeholder="Enter any optional notes"></textarea>
+    <textarea name="notes" id="notes" rows="2" class="form-control" placeholder="Enter any optional notes"></textarea>
   </div>
   <div class="form-group col-md-5">
     <label for="owner">Original Recipe Author</label>
-    <input type="text" class="form-control" id="owner" placeholder="Enter the original author of the recipe if known" />
+    <input type="text" class="form-control" name="owner" id="owner" placeholder="Enter the original author of the recipe if known" />
   </div>
   <div class="form-group col-md-5 col-md-offset-2">
     <label for="submitter">Submitter</label>
-    <input required type="text" class="form-control" id="submitter" placeholder="Enter your name" />
+    <input required type="text" class="form-control" name="submitter" id="submitter" placeholder="Enter your name" />
   </div>
   <div class="form-group col-md-4 col-md-offset-4">
-    <input type="submit" class="btn" value="Submit Recipe" />
+    <input type="submit" name="submit" class="btn" value="Submit Recipe" />
   </div>
 </form>
 
