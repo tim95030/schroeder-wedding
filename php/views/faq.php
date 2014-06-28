210957d5f70cf6c6ab0b0dcd0f99c8f536f9b631
Tim Schroeder <timschroeder@vidder.com>
2014-06-27 22:36:51 -0700

    Some initial answers

--- php/views/faq.php
+++ php/views/faq.php
@@ -8,27 +8,27 @@
 $faqs = array(
   "guests" => array(
     "question" => "Can I bring guests?",
-    "answer" => ""
+    "answer" => "Unfortunately we will be unable to accomadate any extra guests. We have done our best to allow for guests where we could. Please check your invite for your number. If you have any further questions you can call or email us."
     ),
   "kids" => array(
     "question" => "Can I bring my kids?",
-    "answer" => ""
+    "answer" => "We are very limited on space and for the same reason not everyone was given a plus one we cannot accomadate kids."
     ),
   "reception" => array(
     "question" => "Where is the reception/ceremony?",
-    "answer" => ""
+    "answer" => "The reception will be at the African Savanna exhibit at the San Francisco Zoo. Following that there will be a cocktail hour at the Lemur Boardwalk. Then there will have the dinner reception at the Lemur Lounge."
     ),
   "transport" => array(
     "question" => "How do I get there?",
-    "answer" => ""
+    "answer" => "The San Francisco Zoo is at 1 Zoo Road, San Francisco, CA. Click <a href=\"http://www.sfzoo.org/visit/directions.htm\">here for directions</a>"
     ),
   "parking" => array(
     "question" => "Where do I park?",
-    "answer" => ""
+    "answer" => "The main zoo parking lot has two entrances, one off of the Great Highway for people driving from the East Bay and South Bay, and one off of Sloat Boulevard at 47th Avenue for those driving from the North Bay. There is only one exit from the parking lot - putting everyone northbound on the Great Highway."
     ),
   "time" => array(
     "question" => "What time do things happen?",
-    "answer" => ""
+    "answer" => "The ceremony will start 5pm and the reception will continue until 11pm."
     ),
   "ontime" => array(
     "question" => "Do I have to be there on time?",
@@ -36,27 +36,27 @@ $faqs = array(
     ),
   "outside" => array(
     "question" => "Will it take place outside or inside?",
-    "answer" => ""
+    "answer" => "The ceremony and the cocktail hour will be outside. However, the reception will be indoors."
     ),
   "weather" => array(
     "question" => "What will the weather be like?",
-    "answer" => ""
+    "answer" => "Historically the weather in September at the zoo is about 70F with a low of 55F. We will update this with a more accurate estimate when the date is closer."
     ),
   "dresscode" => array(
     "question" => "What is the dress code?",
-    "answer" => ""
+    "answer" => "While our wedding is taking place at a zoo we expect you dress up as we won't be holding back."
     ),
   "rsvp" => array(
     "question" => "Do I really have to RSVP?",
-    "answer" => ""
+    "answer" => "Will we let you in if you don't RSVP? Maybe. But, really it's a few seconds of your life. You can use the form on the <a href=\"/RSVP\">RSVP page</a> and be all done in just moments. We really appreciate your efforts in helping us make this event as easy as possible."
     ),
   "rsvp-deadline" => array(
     "question" => "When is the RSVP deadline?",
-    "answer" => ""
+    "answer" => "I suppose technically September 12th at 4:59pm, but it would help us out a whole lot if you could try to respond before August 1st so we can plan on your company."
     ),
   "rsvp-late" => array(
     "question" => "What if I don't RSVP on time?",
-    "answer" => ""
+    "answer" => "Then we will disown you and never speak to you again. Or nothing. We leave the gambling to you."
     ),
   "lodging" => array(
     "question" => "Where can I stay before and after the wedding?",
@@ -64,7 +64,7 @@ $faqs = array(
     ),
   "food" => array(
     "question" => "Will food be served?",
-    "answer" => ""
+    "answer" => "It's a Wedding what kind of wedding doesn't have food? There will be lots of good food. Starting with hors d'oeuvres and drinks during the cocktail hour, and then a dinner buffet, and finally wedding cake of course."
     ),
   "food-type" => array(
     "question" => "What kind of food will there be?",
@@ -72,11 +72,11 @@ $faqs = array(
     ),
   "dietary" => array(
     "question" => "What if I have a dietary restriction?",
-    "answer" => ""
+    "answer" => "Please contact us with any dietary restrictions and we will see what we can do."
     ),
   "dancing" => array(
     "question" => "Will there be dancing?",
-    "answer" => ""
+    "answer" => "Of course there will be dancing."
     ),
   "not-dancing" => array(
     "question" => "What other activities will there be?",
@@ -84,11 +84,11 @@ $faqs = array(
     ),
   "pictures" => array(
     "question" => "Can I take pictures?",
-    "answer" => ""
+    "answer" => "While we would like there to be no devices used during the ceremony we fully expect you take as many pictures as you can. We even setup a photo sharing account for the occasion. (TODO: ADD DETAILS)"
     ),
   "questions" => array(
     "question" => "I have more questions how can I get answers?",
-    "answer" => ""
+    "answer" => "Please contact us at (TODO: fill out) with any further questions."
     ),
 );
 
