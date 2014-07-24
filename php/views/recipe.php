c57ed2447bbb0d87cffc21d54c1db82f97902a92
Tim Schroeder <timschroeder@vidder.com>
2014-07-23 22:12:10 -0700

    Add description to recipe page

--- php/views/recipe.php
+++ php/views/recipe.php
@@ -1,6 +1,8 @@
 <?php echo $header; ?>
 <h1>Recipe</h1>
-
+<br/>
+<p>Help us build a family cookbook with recipes you love, that you share with your family and that have any special significance to you. If there is a significance to the recipe, please include any accompanying stories in the "notes" section below. After the wedding, we will be binding the recipes into a wedding cookbook for us to enjoy for the rest of our lives. Thank you so much for sharing with us!</p>
+<br/>
 <div class="row">
   <div class="col-md-10 col-md-offset-1">
     <span><?php echo $recipe['title']; ?></span>
